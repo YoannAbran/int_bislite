fc8ce9b753a599cf9457744fe17c4b94a62d97a6
Alexa Vermenot <alexa.vermenot@orange.fr>
2020-05-15 17:02:20 +0200

    modif partie4

--- body-section-4.php
+++ body-section-4.php
@@ -2,45 +2,44 @@
 
 <div  class="container" id="section-4">
   <div class="card-group">
-    <div class="card">
+    <div class="card card h-30">
 
       <div class="card-body">
-        <h5 class="card-title"><img src="img/logo-chat" alt="logo-fleche">TESTIMONIALS</h5>
+        <h5 class="card-title text-uppercase"><img src="img/logo-chat" alt="logo-fleche">testimonials</h5>
           <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p class="card-text"><em> John Travis, CEO, <a href="#">DomainName.com</a></em></p>
       </div>
     </div>
-    <div class="card">  <!-- partie à retravailler ou carrement à finir-->
-      <div class="card-body">
-        <h5 class="card-title"><img src="img/logo-clients" alt="logo-clients">our clients</h5>
+    <div class="card h-30" >
+      <div class="card-body mb-2">
+        <h5 class="card-title text-uppercase"><img src="img/logo-clients" alt="logo-clients">our clients</h5>
+        <div class="container h-100 p-0 m-0" id="logo-clients">
 
-          <div class="d-flex justify-content-center align-items-start">
-              <div class="col-sm col-md-6 borser"><img src="img/cnn" alt="logo-clients"></div>
-              <div class="col-sm col-md-6"><img src="img/cox" alt="logo-clients"></div>
-              <div class="col-sm col-md-6"><img src="img/deere" alt="logo-clients"></div>
-            </div>
-          <div class="d-flex justify-content-center align-items-start">
-              <div class="col-sm col-md-6"><img src="img/apartfinder" alt="appartfinder"></div>
-              <div class="col-sm col-md-6"><img src="img/bananaboat" alt="bananaboat"></div>
-              <div class="col-sm col-md-6"><img src="img/fujifilm" alt="fujifilm"></div>
+          <div class="d-flex flex-row w-100 h-50 d-flex justify-content-around border">
+            <div class="col-sm-4 justify-content-center align-intems-center align-self-center"><a href="#"><img class="img-responsive w-100
+              h-auto"src="img/cnn" alt="logo-clients"/></a></div>
+            <div class="col-sm-4 justify-content-center align-intems-center align-self-center"><a href="#"><img class="img-responsive w-100 h-auto" src="img/cox" alt="logo-clients"></a></div>
+            <div class="col-sm-4 justify-content-center align-intems-center align-self-center"><a href="#"><img class="img-responsive w-100 h-auto" src="img/deere" alt="logo-clients"></a></div>
+          </div>
+          <div class="d-flex flex-row w-100 h-50 d-flex justify-content-around border shadow">
+            <div class="col-sm-4 justify-content-center align-intems-center align-self-center"><a href="#"><img class="img-responsive w-100 h-auto" src="img/apartfinder" alt="logo-clients"></a></div>
+            <div class="col-sm-4 justify-content-center align-intems-center align-self-center"><a href="#"><img class="img-responsive w-100 h-auto" src="img/bananaboat" alt="logo-clients"></a></div>
+            <div class="col-sm-4 justify-content-center align-intems-center align-self-center"><a href="#"><img class="img-responsive w-100 h-auto" src="img/fujifilm" alt="logo-clients"></a></div>
           </div>
 
+        </div>
       </div>
     </div>
-  </div>   <!-- partie à retravailler ou carrement à finir-->
 
-  <div class="jumbotron border p-3">
-    <div class="d-flex justify-content-between align-items-center" id="download">
-      <div class="col-lg-9">
+  <div class="jumbotron border p-3 mt-4">
+    <div class="d-flex row-flex justify-content-between align-items-center" id="download">
+      <div class="col-sm-9 col-xs-12">
         <p class="lead">This is a clean and modern, four column website PSD template. You can code it into a Wordpress website, HTML5 responsive website for your personal or client works. So ahead and download this wonderful PSD template!</p>
       </div>
-      <div class="col-lg-3" id="downloadbutton">
-        <a class="btn btn-lg " href="#" role="button"><img src="img/logo-download1" alt="logo download">&nbsp DOWNLOAD PSD</a>
-
+      <div class="col-sm-3 col-xs-12" id="downloadbutton">
+        <a class="btn btn-sm-xs  " href="#" role="button"><img src="img/logo-download" alt="logo download">&nbsp DOWNLOAD PSD</a>
       </div>
     </div>
-
-
   </div>
 
 </div>
--- footer.php
+++ footer.php
@@ -7,13 +7,13 @@
     <div class="row w-100">
       <div class="col-sm col-md-3" id="cart1">
         <div class="card-body footer">
-          <h5 class="card-title">ABOUT US</h5>
+          <h5 class="card-title text-uppercase">ABOUT US</h5>
           <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi euismod placerat dui et tincidunt. Sed sollicitudin posuere auctor. Phasellus at ultricies nisl. Integer at leo eros. Ut nec lorem id orci convallis porta. Donec pharetra neque eu velit dictum molestie.</p>
         </div>
       </div>
       <div class="col-sm col-md-2" cart="cart2">
         <div class="card-body footer">
-          <h5 class="card-title">EXPLORE</h5>
+          <h5 class="card-title text-uppercase">EXPLORE</h5>
           <a href="#">Home</a><br/>
           <a href="#">About us</a><br/>
           <a href="#">Services</a><br/>
@@ -23,7 +23,7 @@
       </div>
       <div class="col-sm col-md-2">
         <div class="card-body footer">
-          <h5 class="card-title">BROWSE</h5>
+          <h5 class="card-title text-uppercase">BROWSE</h5>
           <a href="#">Career</a><br/>
           <a href="#">Press & Media</a><br/>
           <a href="#">Contact us </a><br/>
@@ -39,16 +39,16 @@
       </div>
       <div class="col-sm col-md-3">
         <div class="card-body footer">
-          <h5 class="card-title">CONNECT WITH US</h5>
+          <h5 class="card-title text-uppercase">CONNECT WITH US</h5>
           <div class="d-flex p-2 justify-content-between align-self-center">
-            <img src="img/facebook" alt="facebook">
-            <img src="img/driible" alt="driible">
-            <img src="img/pinterest" alt="pinterest">
+            <img src="img/facebook" alt="facebook" class="animlogo">
+            <img src="img/driible" alt="driible" class="animlogo">
+            <img src="img/pinterest" alt="pinterest" class="animlogo">
           </div>
           <div class="d-flex p-2 justify-content-between align-self-center">
-            <img src="img/linkedin" alt="linkedin">
-            <img src="img/skype" alt="skype">
-            <img src="img/share" alt="share">
+            <img src="img/linkedin" alt="linkedin" class="animlogo">
+            <img src="img/skype" alt="skype" class="animlogo">
+            <img src="img/share" alt="share" class="animlogo">
           </div>
         </div>
       </div>
@@ -58,7 +58,7 @@
   <div class="container">
     <div class="row align-items-center" id="copyright">
       <div class="col-sm col-md-10 h-5"><span>© Copyright 2012 - BisLite Inc. All rights reserved. Some free icons used here are created by Brankic1979.com.</br>Client Logos are copyright and trademark of the respective owners / companies.<span></div>
-      <div class="col-sm col-md-2"><img src= "img/toplogo.png" alt="Logo"/></div>
+      <div class="col-sm col-md-2"><img class="w-100" src= "img/toplogo.png" alt="Logo"/></div>
     </div>
   </div>
 </div>
