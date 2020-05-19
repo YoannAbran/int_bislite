935f058bba8c1a7911668dcf0fd1b6861cacd026
Alexa Vermenot <alexa.vermenot@orange.fr>
2020-05-19 19:51:18 +0200

    carousel bootrap uniquement

--- body-section-carousel-image.php
+++ body-section-carousel-image.php
@@ -19,37 +19,93 @@
 
 
 
-      <div class="carousel-inner W-100 border justify-content-between">
-        <div class="carousel-item active">
-          <div class="d-flex w-100">
-            <img src="img/im1.png" class="d-block p-4 flex-fill img-fluid" alt="im1">
-            <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
-            <img src="img/im3.png" class="d-block p-4 flex-fill img-fluid" alt="im3">
-            <img src="img/im4.png" class="d-block p-4 flex-fill img-fluid" alt="im4">
+      <div class="carousel-inner">
+        <div class="carousel-item active" id="slide1">
+          <div class="d-none d-lg-block">
+            <div class="d-flex w-100 border justify-content-between">
+              <img src="img/im1.png" class="d-block p-4 flex-fill img-fluid" alt="im1">
+              <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
+              <img src="img/im3.png" class="d-block p-4 flex-fill img-fluid" alt="im3">
+              <img src="img/im4.png" class="d-block p-4 flex-fill img-fluid" alt="im4">
+            </div>
+          </div>
+          <div class="d-none d-md-block d-lg-none">
+            <div class="d-flex w-100 border justify-content-between">
+              <img src="img/im1.png" class="d-block p-4 flex-fill img-fluid" alt="im1">
+              <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
+              <img src="img/im3.png" class="d-block p-4 flex-fill img-fluid" alt="im3">
+            </div>
+          </div>
+          <div class="d-none d-sm-block d-md-none">
+            <div class="d-flex w-100 border justify-content-center">
+              <img src="img/im1.png" class="d-block p-4 flex-fill img-fluid" alt="im1">
+            </div>
           </div>
         </div>
-        <div class="carousel-item">
-          <div class="d-flex w-100">
-            <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
-            <img src="img/im3.png" class="d-block p-4 flex-fill img-fluid" alt="im3">
-            <img src="img/im4.png" class="d-block p-4 flex-fill img-fluid" alt="im4">
-            <img src="img/im1.png" class="d-block p-4 flex-fill img-fluid" alt="im1">
+        <div class="carousel-item" id="slide2">
+          <div class="d-none d-lg-block">
+            <div class="d-flex w-100 border justify-content-between">
+              <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
+              <img src="img/im3.png" class="d-block p-4 flex-fill img-fluid" alt="im3">
+              <img src="img/im4.png" class="d-block p-4 flex-fill img-fluid" alt="im4">
+              <img src="img/im1.png" class="d-block p-4 flex-fill img-fluid" alt="im1">
+            </div>
+          </div>
+          <div class="d-none d-md-block d-lg-none">
+            <div class="d-flex w-100 border justify-content-between">
+              <img src="img/im4.png" class="d-block p-4 flex-fill img-fluid" alt="im4">
+              <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
+              <img src="img/im3.png" class="d-block p-4 flex-fill img-fluid" alt="im3">
+            </div>
+          </div>
+          <div class="d-xs-block d-sm-block d-md-none">
+              <div class="d-flex w-100 border justify-content-between">
+                <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
+              </div>
           </div>
         </div>
-        <div class="carousel-item">
-          <div class="d-flex w-100">
-            <img src="img/im3.png" class="d-block p-4 flex-fill img-fluid" alt="im3">
-            <img src="img/im4.png" class="d-block p-4 flex-fill img-fluid" alt="im4">
-            <img src="img/im1.png" class="d-block p-4 flex-fill img-fluid" alt="im1">
-            <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
+        <div class="carousel-item" id="slide3">
+          <div class="d-none d-lg-block">
+            <div class="d-flex w-100 border justify-content-between">
+              <img src="img/im3.png" class="d-block p-4 flex-fill img-fluid" alt="im3">
+              <img src="img/im4.png" class="d-block p-4 flex-fill img-fluid" alt="im4">
+              <img src="img/im1.png" class="d-block p-4 flex-fill img-fluid" alt="im1">
+              <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
+            </div>
+          </div>
+          <div class="d-none d-md-block d-lg-none">
+            <div class="d-flex w-100 border justify-content-between">
+              <img src="img/im4.png" class="d-block p-4 flex-fill img-fluid" alt="im4">
+              <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
+              <img src="img/im3.png" class="d-block p-4 flex-fill img-fluid" alt="im3">
+            </div>
+          </div>
+          <div class="d-xs-block d-sm-block d-md-none">
+              <div class="d-flex w-100 border justify-content-center">
+                <img src="img/im3.png" class="d-block p-4 flex-fill img-fluid" alt="im3">
+              </div>
           </div>
         </div>
-        <div class="carousel-item">
-          <div class="d-flex w-100">
-            <img src="img/im4.png" class="d-block p-4 flex-fill img-fluid" alt="im4">
-            <img src="img/im1.png" class="d-block p-4 flex-fill img-fluid" alt="im1">
-            <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
-            <img src="img/im3.png" class="d-block p-4 flex-fill img-fluid" alt="im3">
+        <div class="carousel-item" id="slide4">
+          <div class="d-none d-lg-block">
+            <div class="d-flex w-100 border justify-content-between">
+              <img src="img/im4.png" class="d-block p-4 flex-fill img-fluid" alt="im4">
+              <img src="img/im1.png" class="d-block p-4 flex-fill img-fluid" alt="im1">
+              <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
+              <img src="img/im3.png" class="d-block p-4 flex-fill img-fluid" alt="im3">
+            </div>
+          </div>
+          <div class="d-none d-md-block d-lg-none">
+            <div class="d-flex w-100 border justify-content-between">
+              <img src="img/im4.png" class="d-block p-4 flex-fill img-fluid" alt="im4">
+              <img src="img/im1.png" class="d-block p-4 flex-fill img-fluid" alt="im1">
+              <img src="img/im2.png" class="d-block p-4 flex-fill img-fluid" alt="im2">
+            </div>
+          </div>
+          <div class="d-xs-block d-sm-block d-md-none">
+              <div class="d-flex w-100 border justify-content-center">
+                <img src="img/im4.png" class="d-block p-4 flex-fill img-fluid" alt="im4">
+              </div>
           </div>
         </div>
       </div>
