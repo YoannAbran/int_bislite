248ce7c4b7dced79826f5733cc9b21ab27dd99ed
Yoann Abran <y.abran@codeur.online>
2020-05-26 13:54:04 +0200

    carousel

--- body-section-carousel.php
+++ body-section-carousel.php
@@ -2,24 +2,33 @@
 
 <div class="container">
 
+  <div id="slider">
+    <input type="radio" id="show_slide1" name="slider_commands">
+    <input type="radio" id="show_slide2" name="slider_commands">
+    <input type="radio" id="show_slide3" name="slider_commands">
+    <input type="radio" id="show_slide4" name="slider_commands">
+    <input type="radio" id="show_slide5" name="slider_commands">
 
-<div id="slider">
 
-<div id="slides">
-  <figure>
-    <img src="img/cafe.png" alt>
-    <img src="img/img-2.png" alt>
-    <img src="img/img-3.png" alt>
-    <img src="img/img-4.png" alt>
-    <img src="img/img-5.png" alt>
-  </figure>
-</div>
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span>
-  <span class="dot" onclick="currentSlide(2)"></span>
-  <span class="dot" onclick="currentSlide(3)"></span>
-  <span class="dot" onclick="currentSlide(4)"></span>
-  <span class="dot" onclick="currentSlide(5)"></span>
-</div>
-</div>
+    <div id="slides">
+      <figure>
+        <img id="slide1" src="img/cafe.png" alt>
+        <img id="slide2" src="img/img-2.png" alt>
+        <img id="slide3" src="img/img-3.png" alt>
+        <img id="slide4" src="img/img-4.png" alt>
+        <img id="slide5" src="img/img-5.png" alt>
+      </figure>
+    </div>
+
+    <nav>
+      <ul class="dots_commands">
+        <li><label for="show_slide1">S1</label></li>
+        <li><label for="show_slide2">S2</label></li>
+        <li><label for="show_slide3">S3</label></li>
+        <li><label for="show_slide4">S4</label></li>
+        <li><label for="show_slide5">S5</label></li>
+
+      </ul>
+    </nav>
+  </div>
 </div>
