254030f98e755c5264f329a649c98f7b4b1d4def
Yoann Abran <y.abran@codeur.online>
2020-05-28 14:36:03 +0200

    fontawesome

--- body-section-gallery.php
+++ body-section-gallery.php
@@ -3,30 +3,29 @@
 
 
   <div id="gallery" class="">
-    <div id="textgallery" class="">
-      <div><p><i class="fas fa-mouse-pointer"></i> <h2>Latest Works</h2></p></div> <div id="galleryarrow"><p> <i class="fas fa-chevron-circle-left"></i>III<i class="fas fa-chevron-circle-right"></i></p>
-    </div>
+    <div  class="">
+      <div id="textgallery"><p><i class="fas fa-mouse-pointer"></i> <h2>Latest Works</h2></p></div> 
     </div>
   <div id="blockgallery" class="">
       <div id="imglallery1">
         <div class="contimg">
           <img class="imagehov" src="img/im1.png" alt="">
-          <div class="overlay"><a href="#"><i class="fas fa-eye"></i><i class="fas fa-link"></i></a></div>
+          <div class="overlay"><a href="#"><i class="fas fa-eye"></i></a><a href="#"><i class="fas fa-link"></i></a></div>
         </div>
         <div class="contimg">
           <img  class="imagehov"src="img/im2.png" alt="">
-          <div class="overlay"><a href="#"><i class="fas fa-eye"></i><i class="fas fa-link"></i></a></div>
+          <div class="overlay"><a href="#"><i class="fas fa-eye"></i></a><a href="#"><i class="fas fa-link"></i></a></div>
 
         </div>
       </div>
       <div id="imglallery2">
         <div class="contimg">
           <img  class="imagehov"src="img/im3.png" alt="">
-          <div class="overlay"><a href="#"><i class="fas fa-eye"></i><i class="fas fa-link"></i></a></div>
+          <div class="overlay"><a href="#"><i class="fas fa-eye"></i></a><a href="#"><i class="fas fa-link"></i></a></div>
         </div>
         <div class="contimg">
           <img class="imagehov" src="img/im4.png" alt="">
-          <div class="overlay"><a href="#"><i class="fas fa-eye"></i><i class="fas fa-link"></i></a></div>
+          <div class="overlay"><a href="#"><i class="fas fa-eye"></i></a><a href="#"><i class="fas fa-link"></i></a></div>
         </div>
       </div>
     </div>
